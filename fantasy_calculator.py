3a318668189fc908d9f4301a8e475af290a392c3
Hugo Newham <hugo@digitalchameleon.ai>
2022-11-11 14:26:24 +1100

    improved queries

--- fantasy_calculator.py
+++ fantasy_calculator.py
@@ -3,3 +3,18 @@
 #ADP (average draft place) vs current rank, take a look at the most important areas of performance
 #e.g touchdowns vs yards vs receptions.
 
+def calculate_mean():
+    return
+
+def calculate_median():
+    return
+
+def total_points():
+    return
+
+def rank_players():
+    return
+
+#find which metric generates the most points
+def performance_areas():
+    return
--- fantasy_stats.py
+++ fantasy_stats.py
@@ -26,8 +26,12 @@ def search():
             query += ' AND '
         else:
             query += ' WHERE '
-        query += f' Position = {position}'
+        query += f' Position = "{position}"'
     query += ' ORDER BY Fantasy_points Desc;'
+    #default params to show all not working
+    #if week == 0 and position == 0:
+        #query = 'SELECT * FROM fantasy_score ORDER BY Fantasy_points Desc;'
+    print(query)
     db = get_db()
     fantasy_stats = db.execute(query).fetchall()
     return render_template('fantasy.html', fantasy_stats=fantasy_stats, week=week, position=position)
